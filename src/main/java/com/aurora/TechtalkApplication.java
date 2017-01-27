42c0a6b2fa9029f3647f5c994c753ceb6383f8b5
Atif Saddique <atif@aurorasolutions.io>
2017-01-27 17:26:16 +0500

    first commit

---
+++ src/main/java/com/aurora/TechtalkApplication.java
@@ -0,0 +1,12 @@
+package com.aurora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtalkApplication.class, args);
+	}
+}
