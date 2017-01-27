d4607f78112fc219220e33e86de3ef28a491987a
Atif Saddique <atif@aurorasolutions.io>
2017-01-27 19:34:46 +0500

    Integrate spring profiles

--- src/main/java/com/aurora/TechtalkApplication.java
+++ src/main/java/com/aurora/TechtalkApplication.java
@@ -1,12 +1,17 @@
 package com.aurora;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
-public class TechtalkApplication {
+public class TechtalkApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(TechtalkApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        new SpringApplicationBuilder(TechtalkApplication.class)
+                .profiles("dev")
+                .run(args);
+    }
 }
