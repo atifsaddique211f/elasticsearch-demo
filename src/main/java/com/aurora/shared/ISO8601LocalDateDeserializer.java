1a68ec94d8135044e320508524af8c6bcfbf7695
Atif Saddique <atif@aurorasolutions.io>
2017-02-03 19:01:39 +0500

    Add custom date serializer and deserializer

---
+++ src/main/java/com/aurora/shared/ISO8601LocalDateDeserializer.java
@@ -0,0 +1,32 @@
+package com.aurora.shared;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.joda.time.LocalDate;
+import org.joda.time.format.ISODateTimeFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by atif on 2/3/17.
+ */
+public class ISO8601LocalDateDeserializer extends JsonDeserializer<LocalDate>
+{
+    @Override
+    public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException
+    {
+        JsonToken t = jp.getCurrentToken();
+        if (t == JsonToken.VALUE_STRING)
+        {
+            String str = jp.getText().trim();
+            return ISODateTimeFormat.dateTimeParser().parseLocalDate(str);
+        }
+        if (t == JsonToken.VALUE_NUMBER_INT)
+        {
+            return new LocalDate(jp.getLongValue());
+        }
+        throw ctxt.mappingException(handledType());
+    }
+}
